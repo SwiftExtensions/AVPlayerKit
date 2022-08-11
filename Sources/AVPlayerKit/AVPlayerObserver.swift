35ff5ab89de95459d3eb465d2caa709afcf7eb0f
Algashev Alexander <algashev@mail.ru>
2022-08-11 16:46:24 +0300

    Добавь AVPlayerObserver.swift

---
+++ Sources/AVPlayerKit/AVPlayerObserver.swift
@@ -0,0 +1,68 @@
+//
+//  AVPlayerObserver.swift
+//
+
+import AVFoundation
+
+/**
+ Наблюдатель за плеером.
+ 
+ Наблюдатель держит сильную ссылку на плеер.
+ */
+public class AVPlayerObserver {
+    /**
+     Плеер воспроизведения.
+     */
+    public let player: AVPlayer
+    /**
+     Ссылки наблюдателя за плеером.
+     */
+    private var tokens = [Int : NSKeyValueObservation]()
+    
+    /**
+     Создать наблюдателя за плеером.
+     - Parameter player: Плеер воспроизведения.
+     */
+    public init(player: AVPlayer) {
+        self.player = player
+    }
+    
+    /**
+     Установить наблюдателя.
+     - Parameter keyPath: Ключевой путь от корневого типа к типу результирующего значения.
+     - Parameter options: Значения, которые могут быть возвращены в словаре изменений.
+     - Parameter changeHandler: Блок, вызываемый при изменении значения.
+     */
+    public func observe<Value>(
+        _ keyPath: KeyPath<AVPlayer, Value>,
+        options: NSKeyValueObservingOptions = [],
+        changeHandler: @escaping (AVPlayer, NSKeyValueObservedChange<Value>) -> Void)
+    {
+        self.removeObserver(keyPath)
+        let token = self.player.observe(keyPath, options: options, changeHandler: changeHandler)
+        self.tokens[keyPath.hashValue] = token
+    }
+    /**
+     Удалить наблюдателя для конкретного ключа.
+     - Parameter keyPath: Ключевой путь от корневого типа к типу результирующего значения.
+     */
+    public func removeObserver<Value>(_ keyPath: KeyPath<AVPlayer, Value>) {
+        if let token = self.tokens.removeValue(forKey: keyPath.hashValue) {
+            token.invalidate()
+        }
+    }
+    /**
+     Удалить наблюдателя для всех ключей.
+     */
+    public func invalidate() {
+        self.tokens.values.forEach { $0.invalidate() }
+        self.tokens = [:]
+    }
+    
+    deinit {
+        self.invalidate()
+    }
+    
+    
+    
+}
