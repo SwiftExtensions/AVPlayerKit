02bcef5178ff9a6a9336836af07228e87fa169b8
Algashev Alexander <algashev@mail.ru>
2024-05-21 14:11:02 +0300

    Добавь AVURLAssetHTTPHeaderFieldsKey

---
+++ Sources/AVPlayerKit/AVURLAsset.swift
@@ -0,0 +1,13 @@
+//
+//  AVURLAsset.swift
+//
+
+/**
+ Пример:
+ ```swift
+ let url: URL = // A remote asset URL.
+ let options = [AVURLAssetHTTPHeaderFieldsKey: ["HeaderKey": "HeaderValue"]]
+ let asset = AVURLAsset(url: url, options: options)
+ ```
+ */
+public let AVURLAssetHTTPHeaderFieldsKey = "AVURLAssetHTTPHeaderFieldsKey"
