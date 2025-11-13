a9df775faaa5f7c02db24d87eacc479b123cadcc
Algashev Alexander <algashev@mail.ru>
2025-11-13 15:40:34 +0300

    Обнови документирование PlayerView.swift

--- Sources/AVPlayerKit/PlayerView.swift
+++ Sources/AVPlayerKit/PlayerView.swift
@@ -11,22 +11,32 @@ import UIKit
  поддерживаемый слоем
  [AVPlayerLayer](https://developer.apple.com/documentation/avfoundation/avplayerlayer).
  
- Дополнительную информацию см.:
- [Creating a Movie Player App with Basic Playback Controls](https://developer.apple.com/documentation/avfoundation/media_playback_and_selection/creating_a_movie_player_app_with_basic_playback_controls).
+ ## Пример
+ ```swift
+ let playerView = PlayerView(frame: .zero)
+ playerView.player = AVPlayer(url: videoURL)
+ ```
  */
 open class PlayerView: UIView {
     /**
-     Плеер, визуальный контент которого отображает представление.
+     Плеер для отображения визуального контента представления.
+     - Note: Поддерживает
+     [KVO (Key-Value Observing)](https://developer.apple.com/documentation/swift/using-key-value-observing-in-swift)
+     для отслеживания изменений.
      */
     @objc public dynamic var player: AVPlayer? {
         get { self.playerLayer.player }
         set { self.playerLayer.player = newValue }
     }
-
+    /**
+     Слой, обеспечивающий отображение видеоконтента плеера.
+     */
     public var playerLayer: AVPlayerLayer {
         self.layer as! AVPlayerLayer
     }
-
+    /**
+     Тип слоя, используемого представлением для визуализации медиаконтента.
+     */
     public override class var layerClass: AnyClass {
         AVPlayerLayer.self
     }
